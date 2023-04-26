7a6e39eda4aaf0d3fc9dee2e03a38909fd9a66a5
JaySeiw <130736920+JaySeiw@users.noreply.github.com>
2023-04-26 13:30:22 +0200

    Functions update

--- RRTFunctions.m
+++ RRTFunctions.m
@@ -1,24 +1,26 @@
-
+%function [outputs] = name(inputs); sample
 %% Global variables:
 Xmax
 Ymax
+global RectangleMatrix; 
+RectangleMatrix=readmatrix("RectangleMatrix.csv"); % Rectanglematrix with [i,x,y,w,h]
+global ObstacleMatrix;
+ObstacleMatrix=readmatrix('ObstacleMap.csv'); % Map of obstacles with [x1,y1,x2,y2]
 global Height;
-global Rectanglematrix readmatrix("RectangleMatrix.csv");
-Obstaclematrix;
-Length;
-Nodes;
-Start;
-NodeMatrix;
+Height=height(RectangleMatrix); % Count rows of RectangleMatrix
+global Length;
+Length=3; % L max length for a node to be connected to an other
+global Nodes
+Nodes=1000;
+Start=[4,5,0];
+global NodeMatrix;
+NodeMatrix=zeros(1,3);
+Nodematrix(1,:)=Start; % Add start to nodematrix
 
-%Variables
-% Import ObstacleMatrix and rectanglematrix from ObstacleBuilder
-ObstacleMatrix=readmatrix('ObstacleMap.csv'); %map of obstacles with [x1,y1,x2,y2]
-RectangleMatrix=readmatrix('RectangleMatrix.csv'); %Rectanglematrix with [i,x,y,w,h]
-Height=height(RectangleMatrix); %count rows of matrix
-
-%function [outputs] = name(inputs); sample
-
-while loop node creation
+i=1;
+% While loop node creation
+while 1<Nodes+1 
+    %Function for creating new nodes, 
 function [Xnew, Ynew, Parent,] = Nodecreator( matrix(of all nodes, with start), Length);
 
 function [marker] = IntersectionDetector(Xnew, Ynew, Parent, Obstaclematrix, Height, edgeXobstacle);

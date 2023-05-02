dd7557facaac942efa5bf00f77d23b953c72217c
JaySeiw <130736920+JaySeiw@users.noreply.github.com>
2023-05-02 21:32:35 +0200

    Fixed polyxpoly, moved edges to RRTFunctions
    RRT is functional now, on to RRT*

--- IntersectionDetector.m
+++ IntersectionDetector.m
@@ -1,6 +1,11 @@
-function [intersection] = IntersectionDetector(Xnew, Ynew, Parent, ObstacleMatrix, Height, intersection)
+function [intersection, Test] = IntersectionDetector(Xnew, Ynew, Parent, ObstacleMatrix, Height, intersection)
 global NodeMatrix;
 global RectangleMatrix;
+global edges;
+
+
+
+%% Point in obstacle part
 % set k to 1 as counter of rows starting at the first row
 k=1;
 %going through obstacles within row k
@@ -17,35 +22,26 @@ while k<Height+1
         k=k+1;
     end
 end
-% If marker has not yet been set to 1, then we will check for crossing of veretice through edges
-if intersection==0
-    % edges: a matrix representing all the lines of the obstacles. (size N x 4
-    % in the form N x [x1, y1, x2, y2])
-    % first creating a matrix with all the edges of the obstacles in the form
-    % (x1,y1,x2,y2) to use in the function
-    % preallocate matrix to store edges (multiplied by 4 because each rectangle has 4 edges)
-    edges = zeros(size(RectangleMatrix,1)*4,4);
 
-    for i = 1:size(RectangleMatrix,1)
-        x1 = RectangleMatrix(i,2); % x coordinate of bottom left corner
-        y1 = RectangleMatrix(i,3); % y coordinate of bottom left corner
-        w = RectangleMatrix(i,4); % width of rectangle
-        h = RectangleMatrix(i,5); % height of rectangle
+%% Vertice through obstacle part
 
-        % Extract edges and store in edges matrix
-        edges((i-1)*4+1,:) = [x1,y1,x1+w,y1]; % bottom edge
-        edges((i-1)*4+2,:) = [x1,y1,x1,y1+h]; % left edge
-        edges((i-1)*4+3,:) = [x1+w,y1,x1+w,y1+h]; % right edge
-        edges((i-1)*4+4,:) = [x1,y1+h,x1+w,y1+h]; % top edge
-    end
-
-    % Check if the line between NewNode and ParentNode intersects with any of the lines
-    [intx, inty] = polyxpoly([Xnew, NodeMatrix(Parent,1)], [Ynew, NodeMatrix(Parent,2)], edges(:,1:2), edges(:,3:4));
-    % If there is an intersection point, the line is invalid
-    if isempty(intx)&isempty(inty)
-       intersection=0; 
-    else
-       %intersection=1;
+% If marker has not yet been set to 1, then we will check for crossing of vertice through edges
+if intersection~=1
+    i=1;
+    %count up until last row of edges
+    while i < size(edges,1)+1
+        % Check if the line between NewNode and ParentNode intersects with any of the lines
+        [xi, yi] = polyxpoly([NodeMatrix(Parent,1), Xnew], [NodeMatrix(Parent,2), Ynew], edges(i,[1 3]), edges(i,[2 4]));
+        Test=[xi, yi];
+        % If there is an intersection point, the line is invalid
+        if isempty(Test)
+            intersection=0;
+            i=i+1;
+        else
+            i=size(edges,1)+1;
+            intersection=1;
+            disp('intersection');
+        end
     end
 end
 end

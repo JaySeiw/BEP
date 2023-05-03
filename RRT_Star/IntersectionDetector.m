62da029d33a0e4b6d0a5662ac28491889bf08543
JaySeiw <130736920+JaySeiw@users.noreply.github.com>
2023-05-03 10:42:56 +0200

    Restructure

---
+++ RRT_Star/IntersectionDetector.m
@@ -0,0 +1,47 @@
+function [intersection, Test] = IntersectionDetector(Xnew, Ynew, Parent, ObstacleMatrix, Height, intersection)
+global NodeMatrix;
+global RectangleMatrix;
+global edges;
+
+
+
+%% Point in obstacle part
+% set k to 1 as counter of rows starting at the first row
+k=1;
+%going through obstacles within row k
+while k<Height+1
+    % Check if node is within obstacle
+    Xi=discretize(Xnew,[ObstacleMatrix(k,1),ObstacleMatrix(k,3)])==1;
+    Yi=discretize(Ynew,[ObstacleMatrix(k,2),ObstacleMatrix(k,4)])==1;
+    if Xi&&Yi==1
+        % Set counter to break the while loop, because an intersection has been found
+        k=Height+1;
+        % If node is not in obstacle continue to next row
+        intersection=1;
+    else
+        k=k+1;
+    end
+end
+
+%% Vertice through obstacle part
+
+% If marker has not yet been set to 1, then we will check for crossing of vertice through edges
+if intersection~=1
+    i=1;
+    %count up until last row of edges
+    while i < size(edges,1)+1
+        % Check if the line between NewNode and ParentNode intersects with any of the lines
+        [xi, yi] = polyxpoly([NodeMatrix(Parent,1), Xnew], [NodeMatrix(Parent,2), Ynew], edges(i,[1 3]), edges(i,[2 4]));
+        Test=[xi, yi];
+        % If there is an intersection point, the line is invalid
+        if isempty(Test)
+            intersection=0;
+            i=i+1;
+        else
+            i=size(edges,1)+1;
+            intersection=1;
+            disp('intersection');
+        end
+    end
+end
+end
---
+++ RRT_Star/Nodecreator.m
@@ -0,0 +1,32 @@
+function [Xnew, Ynew, LengthMatrix, Parent] = Nodecreator(Xmax, Ymax, NodeMatrix, Length)
+% Assign random coordinates
+    randXNode=Xmax*rand;
+    randYNode=Ymax*rand;
+    % Locating nearest point
+    %make matrix where [x1-xi y1-yi] with i rows because we are not intereste in points of zero starting with starting point
+    DistanceMatrix=[randXNode randYNode]-NodeMatrix(:,[1 2]);
+    %calculate length from new point to all points
+    LengthMatrix=[sqrt(DistanceMatrix(:,1).^2+DistanceMatrix(:,2).^2)];
+
+    %find smallest length to point
+    ClosestPoint=find(LengthMatrix==min(LengthMatrix));
+    %disp('long');
+    if LengthMatrix(ClosestPoint)>Length
+        %calculate angle
+        theta=atan2(DistanceMatrix(ClosestPoint,2),DistanceMatrix(ClosestPoint,1));
+        %set point in same line, but with length L
+        randXNode=Length*cos(theta)+NodeMatrix(ClosestPoint,1);
+        randYNode=Length*sin(theta)+NodeMatrix(ClosestPoint,2);
+        %make matrix where [x1-xi y1-yi] with i rows because we are not intereste in points of zero starting with starting point
+        DistanceMatrix=NodeMatrix(:,[1 2])-[randXNode randYNode];
+        %calculate length from new point to all points
+        LengthMatrix=[sqrt(DistanceMatrix(:,1).^2+DistanceMatrix(:,2).^2)];
+        %re-find smallest length to point
+        ClosestPoint=find(LengthMatrix==min(LengthMatrix));
+    end
+    Parent=ClosestPoint;
+
+
+%we are left with a point, its parent node and a length matrix
+Xnew=randXNode;
+Ynew=randYNode;
